ba9dad3d12b4e43918d426429eb99acf28b8e3e5
Realmbird <happychristopher777@gmail.com>
2024-01-21 22:44:43 -0800

    Added other game ratings error with not working if different names

--- OpenCritic/OpenCritic/settings.py
+++ OpenCritic/OpenCritic/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "OpenCritic.spiders"
 #USER_AGENT = "OpenCritic (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- OpenCritic/OpenCritic/spiders/gamespider.py
+++ OpenCritic/OpenCritic/spiders/gamespider.py
@@ -10,7 +10,7 @@ class GamespiderSpider(scrapy.Spider):
         # gets all the games
         games = response.css('div.game-name a')
         for game in games:
-            relative_url = response.css('div.game-name a::attr(href)').get()
+            relative_url = game.css('::attr(href)').get()
             if relative_url is not None:
                 yield response.follow(relative_url, callback=self.parse_game_page)
             
@@ -20,14 +20,27 @@ class GamespiderSpider(scrapy.Spider):
             #yield response.follow(next_page_url, callback=self.parse)
     
     def parse_game_page(self, response):
+
         yield {
             'OpenCritic_Rating': response.css('.col-4 img::attr(alt)').get(),
             'TopCritic_Average': response.css('.inner-orb::text').getall()[0],
             'Critics_Recommend': response.css('.inner-orb::text').getall()[1],
             'title': response.css('.mb-0::text').get(),
             'publisher': response.css('.companies span::text').get(),
-            'date': response.css('.platforms span strong::text').get(),
+            'platform': response.css('.platforms span strong::text').getall(),
+            'date': response.css('.platforms::text').get(),
+            
         }
+#
+#'Eurogamer': response.css('.col-lg-5 div .score-bold::text').getall()[0],
+#'IGN': response.css('.col-lg-5 div .score-bold::text').getall()[1],
+#'Easy_Allies': response.css('.col-lg-5 div .score-bold::text').getall()[2],
+#'GamesRadar+': response.css('.col-lg-5 div .score-bold::text').getall()[3],
+#'Metro_GameCentral': response.css('.col-lg-5 div .score-bold::text').getall()[4],
+#'Game_Informer ': response.css('.col-lg-5 div .score-bold::text').getall()[5],
+#'Polygon': response.css('.col-lg-5 div .score-bold::text').getall()[6],
+#'GameSpot': response.css('.col-lg-5 div .score-bold::text').getall()[7],
+
 
 # next page
 #next_page = response.css('a[rel="next"]::attr(href)').get()
