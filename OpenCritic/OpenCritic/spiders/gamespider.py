1a86ef26a72f36859c444745be872230fa7016cb
Realmbird <happychristopher777@gmail.com>
2024-03-25 10:15:52 -0700

    Tryig to rescrape steam

--- OpenCritic/OpenCritic/spiders/gamespider.py
+++ OpenCritic/OpenCritic/spiders/gamespider.py
@@ -5,9 +5,6 @@ class GamespiderSpider(scrapy.Spider):
     name = "gamespider"
     allowed_domains = ["opencritic.com"]
     start_urls = ["https://opencritic.com/browse/all?page=1"]
-    page = 1
-    max = 763
-
     def parse(self, response):
         # gets all the games
         games = response.css('div.game-name a')
@@ -15,20 +12,18 @@ class GamespiderSpider(scrapy.Spider):
             relative_url = game.css('::attr(href)').get()
             if relative_url is not None:
                 yield response.follow(relative_url, callback=self.parse_game_page)
-        
-        self.page += 1
-        next_page = "https://opencritic.com/browse/all?page=" + str(self.page)
-        
-        if self.page <= self.max:
-            if next_page is not None:
-                yield response.follow(next_page, callback=self.parse) 
+            
+        next_page = response.css('a[rel="next"]::attr(href)').get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
     
     def parse_game_page(self, response):
         # Find all review blocks
-        # review_names = response.css('.col-lg-5 div:nth-child(1)::text').getall()
-        # review_scores = response.css('.col-lg-5 div .score-bold::text').getall()
+        review_names = response.css('.col-lg-5 div:nth-child(1)::text').getall()
+        review_scores = response.css('.col-lg-5 div .score-bold::text').getall()
 
-        # reviews = {name.strip(): score.strip() for name, score in zip(review_names, review_scores)}
+        reviews = {name.strip(): score.strip() for name, score in zip(review_names, review_scores)}
 
         game_item = OpencriticItem()
         game_item['OpenCritic_Rating'] = response.css('.col-4 img::attr(alt)').get()
@@ -45,7 +40,7 @@ class GamespiderSpider(scrapy.Spider):
         game_item['publisher'] = response.css('.companies span::text').get()
         game_item['platform'] = response.css('.platforms span strong::text').getall()
         game_item['date'] = response.css('.platforms::text').get()
-        # game_item["reviews"] = reviews
+        game_item["reviews"] = reviews
         yield game_item
         # total 8025 scarpped 7341
 #
